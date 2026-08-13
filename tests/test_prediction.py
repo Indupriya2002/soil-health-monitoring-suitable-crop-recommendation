668e9b4f33cae3972c6badcd0e37b8a27138d18d
Indu Priya M <indum7096@gmail.com>
2026-08-13 17:11:46 +0530

    Add crop prediction function tests

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from src.crop_prediction import predict_crop
+
 
 def test_prediction_input_has_required_columns():
     input_data = pd.DataFrame([{
@@ -25,7 +27,6 @@ def test_prediction_input_has_required_columns():
     assert list(input_data.columns) == required_columns
 
 
-
 def test_prediction_is_valid_crop():
     valid_crops = pd.read_csv("crop_dataset.csv")["label"].unique()
 
@@ -43,6 +44,7 @@ def test_negative_nutrient_values_are_detected():
 
     assert input_data["N"] < 0
 
+
 def test_dataset_has_required_columns():
     df = pd.read_csv("crop_dataset.csv")
 
@@ -57,3 +59,20 @@ def test_dataset_has_required_columns():
     ]
 
     assert all(column in df.columns for column in required_columns)
+
+
+def test_prediction_returns_crop():
+    input_data = pd.DataFrame([{
+        "N": 90,
+        "P": 42,
+        "K": 43,
+        "temperature": 25.5,
+        "humidity": 70,
+        "ph": 6.5,
+        "rainfall": 120
+    }])
+
+    predicted_crop = predict_crop(input_data)
+
+    assert isinstance(predicted_crop, str)
+    assert predicted_crop != ""
